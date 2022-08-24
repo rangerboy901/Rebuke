7503e118207ea15d73f98ebb29bd6fee41a8ed30
JDeWeese <rangerboy901@icloud.com>
2022-08-24 18:36:00 -0500

    Initial Commit

---
+++ Rebuke/RebukeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebukeApp.swift
+//  Rebuke
+//
+//  Created by Joseph Wil;liam DeWeese on 8/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RebukeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
